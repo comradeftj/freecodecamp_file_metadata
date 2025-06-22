9331d298c4571b4a066874f7100c2c6d38944ce9
Louis <louisfiliepetiojansel@gmail.com>
2025-06-22 21:51:57 +0700

    additional features

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+const multer = require('multer');
+const path = require('path');
+require('dotenv').config();
 
 var app = express();
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/uploads');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    // extname -> takes the file's extenion (any string beyond the '.')
+    // file.fieldname -> takes what name the file ws assigned at first upload
+  }
+});
+const upload = multer({ storage: storage })
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +25,13 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.post('/api/fileanalyze', (req, res) => {
-  console.log(req.body)
+app.post('/api/fileanalyse', upload.single("upfile"), (req, res) => {
+  if (req.file) {
+    // req.file.originalName, req.file.size, req.file.mimetype
+    res.json({ name: req.file.originalname, type: req.file.mimetype, size: req.file.size});
+  } else {
+    res.json({ error: 'invalid input' });
+  }
 })
 
 
