6e368ccf4e00af6ca9ae807c04127c5598ef5ac5
Louis <louisfiliepetiojansel@gmail.com>
2025-06-22 20:48:48 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var cors = require('cors');
+require('dotenv').config()
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyze', (req, res) => {
+  console.log(req.body)
+})
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
